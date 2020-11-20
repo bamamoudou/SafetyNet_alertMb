0ac16f9ecbb761108b92924707814953631a726a
bamamoudou <mamoudouba8787@gmail.com>
2020-11-20 17:51:21 +0100

    Person model implementation by unit tests

--- src/main/java/com/safetynet/alert/models/Person.java
+++ src/main/java/com/safetynet/alert/models/Person.java
@@ -1,13 +1,95 @@
 package com.safetynet.alert.models;
 
 public class Person {
-//	private Integer id;
-//	private String firstName;
-//	private String lastName;
-//	private String address;
-//	private String city;
-//	private String zip;
-//	private String phone;
-//	private String email;
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String zip;
+	private String phone;
+	private String email;
+
+	/**
+	 * Auto-increment index to set unique id
+	 */
+	public static int index = 0;
+
+	public Person(String firstName, String lastName, String address, String city, String zip, String phone,
+			String email) {
+		super();
+		this.id = index++;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.zip = zip;
+		this.phone = phone;
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
--- src/test/java/com/safetynet/alert/unitTest/models/PersonTest.java
+++ src/test/java/com/safetynet/alert/unitTest/models/PersonTest.java
@@ -1,7 +1,84 @@
 package com.safetynet.alert.unitTest.models;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import com.safetynet.alert.models.Person;
+
 public class PersonTest {
-	//private Person person;
-	
+	private Person person;
+
+	@BeforeEach
+	void initTest() {
+		person = new Person("John", "Boyd", "1509 Culver St", "Culver", "97451", "841-874-6512", "jaboyd@email.com");
+	}
+
+	@Tag("PersonTest")
+	@Test
+	public void getterTest() {
+		// Arrange
+
+		// Act
+		// Assert
+		assertThat(person.getFirstName()).isEqualTo("John");
+		assertThat(person.getLastName()).isEqualTo("Boyd");
+		assertThat(person.getAddress()).isEqualTo("1509 Culver St");
+		assertThat(person.getCity()).isEqualTo("Culver");
+		assertThat(person.getZip()).isEqualTo("97451");
+		assertThat(person.getPhone()).isEqualTo("841-874-6512");
+		assertThat(person.getEmail()).isEqualTo("jaboyd@email.com");
+
+	}
+
+	@Tag("PersonTest")
+	@Test
+	public void gettersAndSettersTest() {
+		person.setFirstName("Jacob");
+		person.setLastName("Boyd");
+		person.setAddress("1509 Culver St");
+		person.setCity("Culver");
+		person.setZip("97451");
+		person.setPhone("841-874-6513");
+		person.setEmail("drk@email.com");
+
+		assertThat(person.getFirstName()).isEqualTo("Jacob");
+		assertThat(person.getLastName()).isEqualTo("Boyd");
+		assertThat(person.getAddress()).isEqualTo("1509 Culver St");
+		assertThat(person.getCity()).isEqualTo("Culver");
+		assertThat(person.getZip()).isEqualTo("97451");
+		assertThat(person.getPhone()).isEqualTo("841-874-6513");
+		assertThat(person.getEmail()).isEqualTo("drk@email.com");
+
+	}
+
+	@Tag("PersonTest")
+	@Test
+	public void SetPersonAsNull() {
+		person.setFirstName("");
+		person.setLastName("");
+		person.setAddress("");
+		person.setCity("");
+		person.setZip("");
+		person.setPhone("");
+		person.setEmail("");
+
+		assertThat(person.getFirstName()).isEmpty();
+		assertThat(person.getLastName()).isEmpty();
+		assertThat(person.getAddress()).isEmpty();
+		assertThat(person.getCity()).isEmpty();
+		assertThat(person.getZip()).isEmpty();
+		assertThat(person.getPhone()).isEmpty();
+		assertThat(person.getEmail()).isEmpty();
+
+	}
+
+	@AfterEach
+	public void undefineTest() {
+		person = null;
+	}
 
 }
