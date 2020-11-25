418630885c99b3da71f715c48cd5d5e0f855b7d6
bamamoudou <mamoudouba8787@gmail.com>
2020-11-25 16:24:46 +0100

    Implement IMedicalRecordService

---
+++ src/main/java/com/safetynet/alert/interfaces/IMedicalRecordService.java
@@ -0,0 +1,28 @@
+package com.safetynet.alert.interfaces;
+
+import com.safetynet.alert.models.MedicalRecord;
+
+public interface IMedicalRecordService {
+
+	/**
+	 * Add MedicalRecord from HTTP POST
+	 * 
+	 * @param newMedicalRecord
+	 */
+	public String httpPostMedicalRecord(MedicalRecord newMedicalRecord);
+
+	/**
+	 * Update MedicalRecord from HTTP PUT
+	 * 
+	 * @param medicalRecord
+	 */
+	public String httpPutMedicalRecord(MedicalRecord medicalRecord);
+
+	/**
+	 * Delete MedicalRecord from HTTP DELETE
+	 * 
+	 * @param id
+	 */
+	public String httpDeleteMedicalRecord(Integer id);
+
+}
