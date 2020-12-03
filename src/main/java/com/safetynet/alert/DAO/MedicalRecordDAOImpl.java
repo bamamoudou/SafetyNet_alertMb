8354cb96ec64f778a6ffedd273543fe6c2abd499
bamamoudou <mamoudouba8787@gmail.com>
2020-12-03 20:24:52 +0100

    Implement methods which allow add, update and delete MedicalRecord data via unit tests

--- src/main/java/com/safetynet/alert/DAO/MedicalRecordDAOImpl.java
+++ src/main/java/com/safetynet/alert/DAO/MedicalRecordDAOImpl.java
@@ -1,45 +1,147 @@
 package com.safetynet.alert.DAO;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.inject.Singleton;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 import com.safetynet.alert.configation.DatabaseConfigImpl;
 import com.safetynet.alert.models.MedicalRecord;
 
+@Singleton
 public class MedicalRecordDAOImpl implements IMedicalRecordDAO {
-	
+
+	private static final Logger LOGGER = LogManager.getLogger("MedicalRecordDAOImpl");
+
+	private DatabaseConfigImpl databaseConfig;
+
+	private PersonDAOImpl personDAO;
+
+	private List<MedicalRecord> allMedicalRecords;
+
+	public MedicalRecordDAOImpl(DatabaseConfigImpl databaseConfig, PersonDAOImpl personDAO) {
+		super();
+		this.databaseConfig = databaseConfig;
+		this.personDAO = personDAO;
+		this.allMedicalRecords = new ArrayList<>();
+		loadData();
+	}
+
 	@Override
 	public void setDatabaseConfig(DatabaseConfigImpl dataBaseConfig) {
-		
+		this.databaseConfig = dataBaseConfig;
+
 	}
-   
+
 	@Override
 	public MedicalRecord getMedicalRecord(Integer id) {
+		for (MedicalRecord iMedicalRecord : allMedicalRecords) {
+			if (iMedicalRecord.getId().equals(id)) {
+				return iMedicalRecord;
+			}
+		}
 		return null;
-		
+
 	}
-	
+
 	@Override
 	public List<MedicalRecord> getAllMedicalRecords() {
-		return null;
-		
+		return this.allMedicalRecords;
+
 	}
-	
+
 	@Override
 	public boolean addNewMedicalRecord(MedicalRecord medicalRecord) {
-		return false;
-		
+		boolean medicalRecordAdded = false;
+
+		if (personDAO.getPersonsById(medicalRecord.getId()) != null) {
+			this.allMedicalRecords.add(medicalRecord);
+			medicalRecordAdded = true;
+		}
+
+		if (medicalRecordAdded) {
+			LOGGER.info("Medical record has been added");
+		} else {
+			LOGGER.error("The medical record of person n° " + medicalRecord.getId() + ", born on "
+					+ medicalRecord.getBirthdate() + " does not correspond to an existing profile");
+		}
+
+		return medicalRecordAdded;
+
 	}
-	
+
 	@Override
 	public boolean updateMedicalRecord(MedicalRecord medicalRecord) {
-		return false;
-		
+		boolean medicalRecordUpdated = false;
+
+		for (MedicalRecord iMedicalRecord : allMedicalRecords) {
+			if (iMedicalRecord.getId().equals(medicalRecord.getId())) {
+				iMedicalRecord.setBirthdate(medicalRecord.getBirthdate());
+				iMedicalRecord.setMedications(medicalRecord.getMedications());
+				iMedicalRecord.setAllergies(medicalRecord.getAllergies());
+				medicalRecordUpdated = true;
+				break;
+			}
+		}
+
+		if (medicalRecordUpdated) {
+			LOGGER.info("Medical record informations have been updated");
+		} else {
+			LOGGER.error("Medical record doesn't exist in medical records list and could not be updated");
+		}
+
+		return medicalRecordUpdated;
+
 	}
-	
+
 	@Override
 	public boolean deleteMedicalRecord(Integer id) {
-		return false;
-		
+		boolean medicalRecordDeleted = false;
+
+		for (int i = 0; i < allMedicalRecords.size(); i++) {
+			if (allMedicalRecords.get(i).getId().equals(id)) {
+				allMedicalRecords.remove(i);
+				medicalRecordDeleted = true;
+				break;
+			}
+		}
+
+		if (medicalRecordDeleted) {
+			LOGGER.info("Medical record have been deleted");
+		} else {
+			LOGGER.error("Medical record doesn't exist in medical records list and could not be deleted");
+		}
+
+		return medicalRecordDeleted;
+
+	}
+
+	private void loadData() {
+		try {
+			JSONObject data = databaseConfig.openConnection();
+
+			JSONArray medicalRecords = (JSONArray) data.get("medicalrecords");
+
+			for (int i = 0; i < medicalRecords.size(); i++) {
+				JSONObject medicalRecord = (JSONObject) medicalRecords.get(i);
+
+				String birthdate = (String) medicalRecord.get("birthdate");
+				List<String> medications = (List<String>) medicalRecord.get("medications");
+				List<String> allergies = (List<String>) medicalRecord.get("allergies");
+
+				allMedicalRecords.add(new MedicalRecord(allMedicalRecords.size() + 1, birthdate, medications, allergies));
+			}
+			LOGGER.info("Medical records are loaded from data");
+		} catch (Exception e) {
+			LOGGER.error("Data can't be loaded in MedicalRecordDAO : " + e);
+		}
+
+		databaseConfig.closeConnection();
 	}
 
 }
---
+++ src/test/java/com/safetynet/alert/unitTest/DAO/MedicalRecordDAOTest.java
@@ -0,0 +1,186 @@
+package com.safetynet.alert.unitTest.DAO;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.safetynet.alert.DAO.MedicalRecordDAOImpl;
+import com.safetynet.alert.DAO.PersonDAOImpl;
+import com.safetynet.alert.configation.DatabaseConfigImpl;
+import com.safetynet.alert.models.MedicalRecord;
+import com.safetynet.alert.models.Person;
+
+@ExtendWith(MockitoExtension.class)
+public class MedicalRecordDAOTest {
+
+	private MedicalRecordDAOImpl medicalRecordDAO;
+
+	private static PersonDAOImpl personDAO;
+	private static StringBuilder data;
+
+	@Mock
+	private static DatabaseConfigImpl databaseConfig;
+
+	@BeforeAll
+	public static void initClassTest() {
+		data = new StringBuilder();
+		data.append("{\"persons\": [");
+		data.append(
+				"{ \"firstName\":\"John\", \"lastName\":\"Boyd\", \"address\":\"1509 Culver St\", \"city\":\"Culver\", \"zip\":\"97451\", \"phone\":\"841-874-6512\", \"email\":\"jaboyd@email.com\" },");
+		data.append(
+				"{ \"firstName\":\"Jacob\", \"lastName\":\"Boyd\", \"address\":\"1509 Culver St\", \"city\":\"Culver\", \"zip\":\"97451\", \"phone\":\"841-874-6513\", \"email\":\"drk@email.com\" },");
+		data.append(
+				"{ \"firstName\":\"Tenley\", \"lastName\":\"Boyd\", \"address\":\"1509 Culver St\", \"city\":\"Culver\", \"zip\":\"97451\", \"phone\":\"841-874-6512\", \"email\":\"tenz@email.com\" },],");
+		data.append("\"firestations\": [");
+		data.append("{ \"address\":\"1509 Culver St\", \"station\":\"3\" },");
+		data.append("{ \"address\":\"29 15th St\", \"station\":\"2\" },");
+		data.append("{ \"address\":\"834 Binoc Ave\", \"station\":\"3\" },");
+		data.append("{ \"address\":\"644 Gershwin Cir\", \"station\":\"1\" },");
+		data.append("{ \"address\":\"748 Townings Dr\", \"station\":\"3\" }],");
+		data.append("\"medicalrecords\": [");
+		data.append(
+				"{ \"firstName\":\"John\", \"lastName\":\"Boyd\", \"birthdate\":\"03/06/1984\", \"medications\":[\"aznol:350mg\", \"hydrapermazol:100mg\"], \"allergies\":[\"nillacilan\"] },");
+		data.append(
+				"{ \"firstName\":\"Jacob\", \"lastName\":\"Boyd\", \"birthdate\":\"03/06/1989\", \"medications\":[\"pharmacol:5000mg\", \"terazine:10mg\", \"noznazol:250mg\"], \"allergies\":[] },");
+		data.append(
+				"{ \"firstName\":\"Tenley\", \"lastName\":\"Boyd\", \"birthdate\":\"02/18/2012\", \"medications\":[], \"allergies\":[\"peanut\"] },]}");
+	}
+
+	@BeforeEach
+	public void initTest() throws IOException, ParseException {
+		when(databaseConfig.openConnection()).thenReturn((JSONObject) new JSONParser().parse(data.toString()));
+		personDAO = new PersonDAOImpl(databaseConfig);
+		medicalRecordDAO = new MedicalRecordDAOImpl(databaseConfig, personDAO);
+	}
+
+	@Tag("MedicalRecordTest")
+	@Test
+	public void loadDataInDAOConstructorTest() {
+		assertThat(medicalRecordDAO.getAllMedicalRecords().size()).isEqualTo(3);
+	}
+
+	@Tag("MedicalRecordDAOTest")
+	@Test
+	public void getMedicalRecordTest() {
+		assertThat(medicalRecordDAO.getMedicalRecord(1)).isInstanceOf(MedicalRecord.class);
+		assertThat(medicalRecordDAO.getMedicalRecord(1).getBirthdate()).isEqualTo("03/06/1984");
+		assertThat(medicalRecordDAO.getMedicalRecord(1).getMedications().size()).isEqualTo(2);
+		assertThat(medicalRecordDAO.getMedicalRecord(1).getMedications().get(0)).isEqualTo("aznol:350mg");
+		assertThat(medicalRecordDAO.getMedicalRecord(1).getMedications().get(1)).isEqualTo("hydrapermazol:100mg");
+		assertThat(medicalRecordDAO.getMedicalRecord(1).getAllergies().size()).isEqualTo(1);
+		assertThat(medicalRecordDAO.getMedicalRecord(1).getAllergies().get(0)).isEqualTo("nillacilan");
+
+		assertThat(medicalRecordDAO.getMedicalRecord(1)).isInstanceOf(MedicalRecord.class);
+		assertThat(medicalRecordDAO.getMedicalRecord(2).getBirthdate()).isEqualTo("03/06/1989");
+		assertThat(medicalRecordDAO.getMedicalRecord(2).getMedications().size()).isEqualTo(3);
+		assertThat(medicalRecordDAO.getMedicalRecord(2).getMedications().get(0)).isEqualTo("pharmacol:5000mg");
+		assertThat(medicalRecordDAO.getMedicalRecord(2).getMedications().get(1)).isEqualTo("terazine:10mg");
+		assertThat(medicalRecordDAO.getMedicalRecord(2).getMedications().get(2)).isEqualTo("noznazol:250mg");
+		assertThat(medicalRecordDAO.getMedicalRecord(2).getAllergies().size()).isEqualTo(0);
+
+		assertThat(medicalRecordDAO.getMedicalRecord(1)).isInstanceOf(MedicalRecord.class);
+		assertThat(medicalRecordDAO.getMedicalRecord(3).getBirthdate()).isEqualTo("02/18/2012");
+		assertThat(medicalRecordDAO.getMedicalRecord(3).getMedications().size()).isEqualTo(0);
+		assertThat(medicalRecordDAO.getMedicalRecord(3).getAllergies().size()).isEqualTo(1);
+		assertThat(medicalRecordDAO.getMedicalRecord(3).getAllergies().get(0)).isEqualTo("peanut");
+
+	}
+
+	@Tag("MedicalRecordDAOTest")
+	@Test
+	public void getAllMedicalRecordsTest() {
+		assertThat(medicalRecordDAO.getAllMedicalRecords()).isInstanceOf(List.class);
+		assertThat(medicalRecordDAO.getAllMedicalRecords().size()).isEqualTo(3);
+		assertThat(medicalRecordDAO.getAllMedicalRecords().get(0).getId()).isEqualTo(1);
+		assertThat(medicalRecordDAO.getAllMedicalRecords().get(0).getBirthdate()).isEqualTo("03/06/1984");
+		assertThat(medicalRecordDAO.getAllMedicalRecords().get(1).getId()).isEqualTo(2);
+		assertThat(medicalRecordDAO.getAllMedicalRecords().get(1).getBirthdate()).isEqualTo("03/06/1989");
+		assertThat(medicalRecordDAO.getAllMedicalRecords().get(2).getId()).isEqualTo(3);
+		assertThat(medicalRecordDAO.getAllMedicalRecords().get(2).getBirthdate()).isEqualTo("02/18/2012");
+
+		assertThatExceptionOfType(IndexOutOfBoundsException.class)
+				.isThrownBy(() -> medicalRecordDAO.getAllMedicalRecords().get(-1));
+		assertThatExceptionOfType(IndexOutOfBoundsException.class)
+				.isThrownBy(() -> medicalRecordDAO.getAllMedicalRecords().get(3));
+	}
+
+	@Tag("MedicalRecordDAOTest")
+	@Test
+	public void addNewMedicalRecordTest() {
+		assertThat(medicalRecordDAO.getAllMedicalRecords().size()).isEqualTo(3);
+		List<String> newMedicalRecordMedication = new ArrayList<>();
+		List<String> newMedicalRecordAllergies = new ArrayList<>();
+		MedicalRecord newMedicalRecord = new MedicalRecord(personDAO.getAllPersons().size() + 1, "01/01/1920",
+				newMedicalRecordMedication, newMedicalRecordAllergies);
+		assertThat(medicalRecordDAO.addNewMedicalRecord(newMedicalRecord)).isFalse();
+		assertThat(medicalRecordDAO.getAllMedicalRecords().size()).isEqualTo(3);
+
+		Person newPerson = new Person(personDAO.getAllPersons().size() + 1, "Tessa", "Carman", "834 Binoc Ave", "Culver",
+				"97451", "841-874-6512", "tenz@email.com");
+		personDAO.addNewPerson(newPerson);
+
+		assertThat(medicalRecordDAO.addNewMedicalRecord(newMedicalRecord)).isTrue();
+		assertThat(medicalRecordDAO.getAllMedicalRecords().size()).isEqualTo(4);
+	}
+
+	@Tag("MedicalRecordDAOTest")
+	@Test
+	public void updateMedicalRecordTest() {
+		assertThat(medicalRecordDAO.getMedicalRecord(1)).isInstanceOf(MedicalRecord.class);
+		assertThat(medicalRecordDAO.getMedicalRecord(1).getBirthdate()).isEqualTo("03/06/1984");
+		assertThat(medicalRecordDAO.getMedicalRecord(1).getMedications().size()).isEqualTo(2);
+		assertThat(medicalRecordDAO.getMedicalRecord(1).getMedications().get(0)).isEqualTo("aznol:350mg");
+		assertThat(medicalRecordDAO.getMedicalRecord(1).getMedications().get(1)).isEqualTo("hydrapermazol:100mg");
+		assertThat(medicalRecordDAO.getMedicalRecord(1).getAllergies().size()).isEqualTo(1);
+		assertThat(medicalRecordDAO.getMedicalRecord(1).getAllergies().get(0)).isEqualTo("nillacilan");
+
+		assertThat(medicalRecordDAO.getAllMedicalRecords().size()).isEqualTo(3);
+
+		List<String> newMedicationOfMedicalRecord = new ArrayList<>();
+		newMedicationOfMedicalRecord.add("thradox:700mg");
+		List<String> newAllergiesOfMedicalRecord = new ArrayList<>();
+		newAllergiesOfMedicalRecord.add("illisoxian");
+		MedicalRecord newInformationsOfMedicalRecord = new MedicalRecord(1, "08/30/1979", newMedicationOfMedicalRecord,
+				newAllergiesOfMedicalRecord);
+		assertThat(medicalRecordDAO.updateMedicalRecord(newInformationsOfMedicalRecord)).isTrue();
+
+		assertThat(medicalRecordDAO.getMedicalRecord(1)).isInstanceOf(MedicalRecord.class);
+		assertThat(medicalRecordDAO.getMedicalRecord(1).getBirthdate()).isEqualTo("08/30/1979");
+		assertThat(medicalRecordDAO.getMedicalRecord(1).getMedications().size()).isEqualTo(1);
+		assertThat(medicalRecordDAO.getMedicalRecord(1).getMedications().get(0)).isEqualTo("thradox:700mg");
+		assertThat(medicalRecordDAO.getMedicalRecord(1).getAllergies().size()).isEqualTo(1);
+		assertThat(medicalRecordDAO.getMedicalRecord(1).getAllergies().get(0)).isEqualTo("illisoxian");
+
+		assertThat(medicalRecordDAO.getAllMedicalRecords().size()).isEqualTo(3);
+
+		newInformationsOfMedicalRecord.setId(10);
+		assertThat(medicalRecordDAO.updateMedicalRecord(newInformationsOfMedicalRecord)).isFalse();
+
+		assertThat(medicalRecordDAO.getAllMedicalRecords().size()).isEqualTo(3);
+	}
+
+	@Tag("MedicalRecordDAOTest")
+	@Test
+	void deleteMedicalRecord() {
+		assertThat(medicalRecordDAO.getAllMedicalRecords().size()).isEqualTo(3);
+		medicalRecordDAO.deleteMedicalRecord(1);
+		assertThat(medicalRecordDAO.getAllMedicalRecords().size()).isEqualTo(2);
+		assertThat(medicalRecordDAO.deleteMedicalRecord(1)).isFalse();
+	}
+
+}
