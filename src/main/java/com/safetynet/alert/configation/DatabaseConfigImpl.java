93aed60cf5f4faac75f14abed8923caa9aba99f9
bamamoudou <mamoudouba8787@gmail.com>
2020-12-07 20:36:55 +0100

    Implement data configuration

--- src/main/java/com/safetynet/alert/configation/DatabaseConfigImpl.java
+++ src/main/java/com/safetynet/alert/configation/DatabaseConfigImpl.java
@@ -1,28 +1,58 @@
 package com.safetynet.alert.configation;
 
+import java.io.FileReader;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class DatabaseConfigImpl implements IDatabaseConfig {
-	public JSONObject openConnection() {
-		return null;
-		
+
+	private static final Logger LOGGER = LogManager.getLogger("DatabaseConfigImpl");
+
+	private JSONObject data;
+
+	public DatabaseConfigImpl() {
+
 	}
-	
+
+	public JSONObject openConnection() {
+		try (FileReader fileData = new FileReader("src/main/resources/static/data.json")) {
+			this.data = (JSONObject) new JSONParser().parse(fileData);
+			LOGGER.info("Connection open");
+			return this.data;
+		} catch (Exception e) {
+			LOGGER.error("Connection error");
+			return null;
+		}
+
+	}
+
 	@Override
 	public JSONObject openConnection(String filepath) {
-		return null;
-		
+		try (FileReader fileData = new FileReader(filepath)) {
+			this.data = (JSONObject) new JSONParser().parse(fileData);
+			LOGGER.info("Connection open");
+			return this.data;
+		} catch (Exception e) {
+			LOGGER.error("Connection error");
+			return null;
+		}
+
 	}
-	
+
 	@Override
 	public JSONObject getData() {
-		return null;
-		
+		return data;
+
 	}
-	
+
 	@Override
 	public void closeConnection() {
-		
+		this.data = null;
+		LOGGER.info("Connection close");
+
 	}
 
 }
---
+++ src/test/java/com/safetynet/alert/unitTest/configuration/DatabaseConfigTest.java
@@ -0,0 +1,79 @@
+package com.safetynet.alert.unitTest.configuration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import com.safetynet.alert.configation.DatabaseConfigImpl;
+
+public class DatabaseConfigTest {
+
+	private DatabaseConfigImpl databaseConfig;
+
+	@BeforeEach
+	void initTests() {
+		databaseConfig = new DatabaseConfigImpl();
+	}
+
+	@Tag("DatabaseConfigTest")
+	@Test
+	void openConnection() {
+		JSONObject data = databaseConfig.openConnection();
+		assertThat(data).isNotEmpty();
+
+		JSONArray persons = (JSONArray) data.get("persons");
+		JSONArray stations = (JSONArray) data.get("firestations");
+		JSONArray medicalRecords = (JSONArray) data.get("medicalrecords");
+
+		assertThat(persons.size()).isPositive();
+		assertThat(stations.size()).isPositive();
+		assertThat(medicalRecords.size()).isPositive();
+
+		data = null;
+		persons = null;
+		stations = null;
+		medicalRecords = null;
+
+		data = databaseConfig.openConnection("src/main/resources/static/data.json");
+		assertThat(data).isNotEmpty();
+
+		persons = (JSONArray) data.get("persons");
+		stations = (JSONArray) data.get("firestations");
+		medicalRecords = (JSONArray) data.get("medicalrecords");
+
+		assertThat(persons.size()).isPositive();
+		assertThat(stations.size()).isPositive();
+		assertThat(medicalRecords.size()).isPositive();
+
+		assertThat(databaseConfig.openConnection("")).isNullOrEmpty();
+	}
+
+	@Tag("DatabaseConfigTest")
+	@Test
+	void getData() {
+		assertThat(databaseConfig.getData()).isNullOrEmpty();
+		JSONObject data = databaseConfig.openConnection();
+		assertThat(databaseConfig.getData()).isNotEmpty();
+		assertThat(databaseConfig.getData()).isEqualTo(data);
+	}
+
+	@Tag("DatabaseConfigTest")
+	@Test
+	void closeConnection() {
+		assertThat(databaseConfig.getData()).isNullOrEmpty();
+		assertThat(databaseConfig.openConnection()).isNotEmpty();
+		databaseConfig.closeConnection();
+		assertThat(databaseConfig.getData()).isNullOrEmpty();
+	}
+
+	@AfterEach
+	void undefTests() {
+		databaseConfig = null;
+	}
+
+}
