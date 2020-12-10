7cb19df4b53ad70709cba58bd545f6d310dd4149
bamamoudou <mamoudouba8787@gmail.com>
2020-12-10 19:45:29 +0100

    Implement methods which allow get person infos

---
+++ src/main/java/com/safetynet/alert/controllers/InformationController.java
@@ -0,0 +1,68 @@
+package com.safetynet.alert.controllers;
+
+import javax.inject.Singleton;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.safetynet.alert.services.impl.InformationServiceImpl;
+
+@RestController
+@Singleton
+public class InformationController {
+
+	private InformationServiceImpl informationService;
+
+	public InformationController(InformationServiceImpl informationService) {
+		super();
+		this.informationService = informationService;
+	}
+
+	@GetMapping("/firestation")
+	public String getFirestation(@RequestParam(required = false) Integer firestationNumber) {
+
+		return informationService.getAllPersonsServedByTheStationWithCount(firestationNumber);
+
+	}
+
+	@GetMapping("/phoneAlert")
+	public String getPhoneAler(@RequestParam(required = false) Integer firestation) {
+
+		return informationService.getAllPersonsPhoneByStationNumber(firestation);
+
+	}
+
+	@GetMapping("/childAlert")
+	public String getchildAlert(@RequestParam(required = false) String address) {
+
+		return informationService.getAllChildByAddress(address);
+
+	}
+
+	@GetMapping("/fire")
+	public String getFire(@RequestParam(required = false) String address) {
+
+		return informationService.getAllPersonsLivingAtTheAddress(address);
+	}
+
+	@GetMapping("/flood/stations")
+	public String getFlood(@RequestParam(required = false) String firestation) {
+
+		return informationService.getAllPersonsServedByTheStations(firestation);
+
+	}
+
+	@GetMapping("/personInfo")
+	public String getPersonInfo(@RequestParam(required = false) String firstName, String lastName) {
+
+		return informationService.getAllCompleteProfileOfPersonsByName(firstName, lastName);
+	}
+
+	@GetMapping("/communityEmail")
+	public String getCommunityEmail(@RequestParam(required = false) String city) {
+
+		return informationService.getAllPersonsEmailByCity(city);
+	}
+
+}
---
+++ src/test/java/com/safetynet/alert/unitTest/controllers/InformationControllerTest.java
@@ -0,0 +1,74 @@
+package com.safetynet.alert.unitTest.controllers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.safetynet.alert.services.impl.InformationServiceImpl;
+
+@WebMvcTest
+@RunWith(SpringRunner.class)
+@ComponentScan({ "com.safetynet.alert.configuration", "com.safetynet.alert.controller" })
+public class InformationControllerTest {
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@MockBean
+	InformationServiceImpl informationService;
+
+	@Test
+	public void getMappingFirestationTest() throws Exception {
+
+		mockMvc.perform(get("/firestation")).andExpect(status().is2xxSuccessful()).andReturn();
+	}
+
+	@Test
+	public void getChildAlertTest() throws Exception {
+
+		mockMvc.perform(get("/childAlert")).andExpect(status().is2xxSuccessful()).andReturn();
+
+	}
+
+	@Test
+	public void getPhoneAlertTest() throws Exception {
+
+		mockMvc.perform(get("/phoneAlert")).andExpect(status().is2xxSuccessful()).andReturn();
+
+	}
+
+	@Test
+	public void getFireAlertTest() throws Exception {
+
+		mockMvc.perform(get("/fire")).andExpect(status().is2xxSuccessful()).andReturn();
+
+	}
+
+	@Test
+	public void getFloodAlertTest() throws Exception {
+
+		mockMvc.perform(get("/flood/stations")).andExpect(status().is2xxSuccessful()).andReturn();
+	}
+
+	@Test
+	public void getPersonInfoTest() throws Exception {
+
+		mockMvc.perform(get("/personInfo")).andExpect(status().is2xxSuccessful()).andReturn();
+
+	}
+
+	@Test
+	public void getCommunityEmailTest() throws Exception {
+
+		mockMvc.perform(get("/communityEmail")).andExpect(status().is2xxSuccessful()).andReturn();
+	}
+
+}
